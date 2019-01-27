82e680bd2d4e5aac38198f073e9e52d31795249d
Cheng Vue <cvu3@ucsc.edu>
2019-01-27 09:43:50 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,17 +1,32 @@
 class SongsController < ApplicationController
 
   def new
-  end
-
-  def create
+    @genre = Song.new
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(post_params(:name, :user_id, :genre_id))
+    @song.save
+    redirect_to song_path(@song)
   end
 
   def update
+    @song = Song.find(params[:id])
+    @song.update(post_params(:name, :user_id, :genre_id))
+    redirect_to song_path(@song)
+  end
+
+  private
+
+  def post_params(*args)
+    params.require(:song).permit(*args)
   end
 end
