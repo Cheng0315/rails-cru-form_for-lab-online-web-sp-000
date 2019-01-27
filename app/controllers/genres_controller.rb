4fa79e32598ac802f58739a898b62d263fc9b6e2
Cheng Vue <cvu3@ucsc.edu>
2019-01-27 09:38:43 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,17 +1,32 @@
 class GenresController < ApplicationController
 
   def new
-  end
-
-  def create
+    @genre = Genre.new
   end
 
   def show
+    @genre = Artist.find(params[:id])
   end
 
   def edit
+    @genre = Artist.find(params[:id])
+  end
+
+  def create
+    @genre = Artist.new(post_params(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
   end
 
   def update
+    @genre = Artist.find(params[:id])
+    @genre.update(post_params(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  private
+
+  def post_params(*args)
+    params.require(:artist).permit(*args)
   end
 end
