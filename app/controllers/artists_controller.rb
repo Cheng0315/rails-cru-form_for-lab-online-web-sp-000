6e998637a8412918f63501d6d8705caa06bc4ae8
Cheng Vue <cvu3@ucsc.edu>
2019-01-27 09:31:52 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,17 +1,32 @@
 class ArtistsController < ApplicationController
 
   def new
-  end
-
-  def create
+    @artist = Artist.new
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def edit
+    @artist = Student.find(params[:id])
+  end
+
+  def create
+    @artist = Student.new(post_params(:first_name, :last_name))
+    @artist.save
+    redirect_to student_path(@artist)
   end
 
   def update
+    @artist = Student.find(params[:id])
+    @artist.update(post_params(:first_name, :last_name))
+    redirect_to student_path(@artist)
+  end
+
+  private
+
+  def post_params(*args)
+    params.require(:artist).permit(*args)
   end
 end
