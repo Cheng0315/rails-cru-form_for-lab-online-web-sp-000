41b4faa251316238a8b541417c057671db49a215
Cheng Vue <cvu3@ucsc.edu>
2019-01-27 10:08:12 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :songs, only: [:new, :show, :create, :edit, :update]
   resources :genres, only: [:new, :show, :create, :edit, :update]
   resources :artists, only: [:new, :show, :create, :edit, :update]
+  get '/songs' => 'index#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
