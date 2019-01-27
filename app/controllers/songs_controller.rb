7276674cb11ff682b2cc96f4ede3f5572941647c
Cheng Vue <cvu3@ucsc.edu>
2019-01-27 10:31:19 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,7 +26,7 @@ class SongsController < ApplicationController
 
   def update
     @song = Song.find(params[:id])
-    @song.update(post_params(:name, :user_id, :genre_id))
+    @song.update(post_params(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
 
